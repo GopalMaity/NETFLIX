a13d2f6909f9d7c16a306a4cf340494460cd6cb9
Gopal Maity <maityg74@gmail.com>
2023-03-30 02:58:58 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import "./app.scss"
+ import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <Home/>
+      <Login/>
+      <Register/>
+      <Watch/>
+    </div>
+  )
 };
 
 export default App;
